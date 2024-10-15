87c95ebc0295a2e3e98018d321683ddc7118e578
coffeeBeansz <tomas.pousette@gmail.com>
2024-10-15 13:03:30 +0200

    fix: updated with timezone in logging

--- log_current_date.py
+++ log_current_date.py
@@ -1,8 +1,10 @@
 import datetime
+import pytz
 
 def main():
+    timezone = pytz.timezone('Europe/Stockholm')
     with open('log.txt', 'a') as f:
-        f.write(f'Writing to log at {datetime.datetime.now()}\n')
+        f.write(f'Writing to log at {datetime.datetime.now(timezone)}\n')
 
 if __name__ == '__main__':
     main()
